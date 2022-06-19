62e7795d1999362361f9fa592a925b488c374e20
krolyxon <krolyxon@tutanota.com>
2022-06-20 02:16:54 +0530

    return to monke colors

--- config.h
+++ config.h
@@ -14,10 +14,9 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-
-	[SchemeNorm] = { "#a89984", "#282828" },
-	[SchemeSel] = { "#282828", "#d3a256" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+      	[SchemeNorm] = { "#bbbbbb", "#222222" },
+      	[SchemeSel] = { "#eeeeee", "#005577" },
+      	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
