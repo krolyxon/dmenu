e6dbeb47f24f70b9994166024c8db10567562dc9
krolyxon <krolyxon@tutanota.com>
2022-07-22 00:16:12 +0530

    changing font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 
 static const char *fonts[] = {
-    "Hack Nerd Font:size=10",
-    "Hack Nerd Font:pixelsize=10:antialias=true:autohint=true",
+    "JetBrains Mono:size=10",
+    "JetBrains Mono:pixelsize=10:antialias=true:autohint=true",
     "emoji:size=10" // For rendering emojis 
 };
 static const unsigned int bgalpha = 0xe0;
